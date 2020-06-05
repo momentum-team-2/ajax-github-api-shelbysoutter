056821d2cbc207b952194574046532a409ac501f
Amy Gori <amydgori@gmail.com>
2020-06-05 12:10:18 -0400

    Use and modify tachyons

--- main.js
+++ main.js
@@ -9,6 +9,12 @@ fetch(url)
     return response.json()
 })
 .then(function (data) {
+    console.log(data)
+    let template = createProfileCard(data)
+    document.querySelector('.target').innerHTML = template
+})
+
+function createDOMElements () {
     let nameLi = document.createElement('li')
     nameLi.innerText = 'Name: ' + data.name
     basicsElement.appendChild(nameLi)
@@ -25,8 +31,27 @@ fetch(url)
     profilePic.innerHTML = createImage(data.avatar_url)
     storyElement.appendChild(profilePic)
     profilePic.classList.add("mw5")
-})
+}
 
 function createImage (url) {
     return `<img src=${url}>`
+}
+
+function createProfileCard (data) {
+    return `
+    <article class="flex-container mw6 center bg-white br3 pa3 pa4-ns mv3 ba b--black-10">
+        <div class="tc flex-container flex-column">
+            <img src=${data.avatar_url} class="br-100 h4 w4 dib" title="Photo of a kitty staring at you">
+            <h1 class="f4">${data.name}</h1>
+        </div>
+        <div class="bio-site pl3">
+            <h3>The Story</h3>
+            <p class="bio-site pb3 lh-copy measure center f6 black-70">
+            ${data.bio}
+            </p>
+            <h3>The Basics</h3>
+            <a href=${data.repos_url}>GitHub Projects</a>
+        </div>
+    </article>
+    `
 }
